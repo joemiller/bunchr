9f0a6f8032c874e4bd868ed637b74934d8d70fb4
Justin Kolberg <amd.prophet@gmail.com>
2015-08-12 17:29:16 -0700

    [freebsd_support] load_plugins must be called in newer versions of ohai

--- lib/bunchr/ohai.rb
+++ lib/bunchr/ohai.rb
@@ -8,6 +8,7 @@ module Bunchr
     def self.ohai
       if @@ohai.nil?
         @@ohai ||= ::Ohai::System.new
+        @@ohai.load_plugins
         @@ohai.require_plugin('os')
         @@ohai.require_plugin('platform')
       end
